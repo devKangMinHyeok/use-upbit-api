b849291aadfec9278c658b1861464690e0be47b5
Kang <rkdalsgur032@unist.ac.kr>
2023-04-06 19:27:16 +0900

    [#7] chore : index.ts set root

---
+++ index.ts
@@ -0,0 +1,6 @@
+import useFetchMarketCode from './src/hooks/useFetchMarketCode';
+import useWsOrderbook from './src/hooks/useWsOrderbook';
+import useWsTrade from './src/hooks/useWsTrade';
+import useWsTicker from './src/hooks/useWsTicker';
+
+export {useFetchMarketCode, useWsOrderbook, useWsTrade, useWsTicker};
--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import useFetchMarketCode from './hooks/useFetchMarketCode';
-import useWsOrderbook from './hooks/useWsOrderbook';
-import useWsTrade from './hooks/useWsTrade';
-import useWsTicker from './hooks/useWsTicker';
-
-export {useFetchMarketCode, useWsOrderbook, useWsTrade, useWsTicker};
